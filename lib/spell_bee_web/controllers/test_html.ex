21101a967992f0e4d0682bbe87a3d4c4ba2a9f12
nizwant <mateusznizwantowski@gmail.com>
2024-01-04 19:04:24 +0100

    creating directory with templates

--- lib/spell_bee_web/controllers/test_html.ex
+++ lib/spell_bee_web/controllers/test_html.ex
@@ -1,15 +1,5 @@
 defmodule SpellBeeWeb.TestHTML do
   use SpellBeeWeb, :html
 
-  def test(assigns) do
-    ~H"""
-    welcome to the summoners rift
-    """
-  end
-
-  def template_with_name(assigns) do
-    ~H"""
-    Welcome <%= @name %> to the summoners rift
-    """
-  end
+  embed_templates "test_html/*"
 end
